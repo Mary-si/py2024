814cf21738aae68310a3008525b7e0e10dc029ca
Mariya <by.zorina08@gmail.com>
2024-05-15 12:29:37 +0300

    I add file with Homework5

--- Homework5.py
+++ Homework5.py
@@ -1,11 +1,17 @@
+# Заменить символ “#” на символ “/” в строке 'www.my_site.com#about'
+
 txt: str = "www.my_site.com#about"
 print(txt.replace("#", "/"))
+
+# Напишите программу, которая добавляет ‘ing’ к словам
 
 a = ["run", "go", "lie"]
 for word in a:
     "{word}ing"
-word_list_ing = " ".join(f"{word}ing" for word in a)
-print(word_list_ing)
+b = " ".join(f"{word}ing" for word in a)
+print(b)
+
+# В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
 
 b = "Ivanou Ivan"
 spaceIndex = b.find(" ")
@@ -13,9 +19,14 @@ word1 = b[:spaceIndex + 1]
 word2 = b[spaceIndex + 1:]
 print(word2 + " " + word1)
 
+# Напишите программу которая удаляет пробел в начале, в конце строки
+
 s = " Hello World! "
 s = s.strip()
 print(s)
 
-txt = "pARiS"
-print(txt.capitalize())
+# Имена собственные всегда начинаются с заглавной буквы, за которой следуют строчные буквы.
+# Исправьте данное имя собственное так, чтобы оно соответствовало этому утверждению. "pARiS" >> "Paris"
+
+a = "pARiS"
+print(a.capitalize())
