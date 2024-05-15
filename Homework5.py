34399b532a2e5c6b820c9cd9f8bd23af7e0f787b
Mariya <by.zorina08@gmail.com>
2024-05-15 12:21:52 +0300

    I add file with Homework5

--- Homework5.py
+++ Homework5.py
@@ -1,16 +1,16 @@
 txt: str = "www.my_site.com#about"
 print(txt.replace("#", "/"))
 
-word_list = ["run", "go", "lie"]
-for word in word_list:
+a = ["run", "go", "lie"]
+for word in a:
     "{word}ing"
-word_list_ing = " ".join(f"{word}ing" for word in word_list)
+word_list_ing = " ".join(f"{word}ing" for word in a)
 print(word_list_ing)
 
-name: str = "Ivanou Ivan"
-spaceIndex = name.find(" ")
-word1 = name[:spaceIndex + 1]
-word2 = name[spaceIndex + 1:]
+b = "Ivanou Ivan"
+spaceIndex = b.find(" ")
+word1 = b[:spaceIndex + 1]
+word2 = b[spaceIndex + 1:]
 print(word2 + " " + word1)
 
 s = " Hello World! "
