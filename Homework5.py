c3dc1d680a86e8a68451c562a93b0f20f1307d69
Mariya <by.zorina08@gmail.com>
2024-05-15 14:14:39 +0300

    I add file with Homework5

--- Homework5.py
+++ Homework5.py
@@ -2,25 +2,25 @@
 SITE = "www.my_site.com#about"
 print(SITE.replace("#", "/"))
 
-""" Напишите программу, которая добавляет ‘ing’ к словам """
-words = ["run", "go", "lie"]
-for word in words:
+# Напишите программу, которая добавляет ‘ing’ к словам
+WORDS = ["run", "go", "lie"]
+for word in WORDS:
     "{word}ing"
-word_ing = " ".join(f"{word}ing" for word in words)
+word_ing = " ".join(f"{word}ing" for word in WORDS)
 print(word_ing)
 
-""" В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou" """
-login: str = "Ivanou Ivan"
-spaceIndex = login.find(" ")
-surname = login[:spaceIndex + 1]
-first_name = login[spaceIndex + 1:]
+# В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
+LOGIN = "Ivanou Ivan"
+spaceIndex = LOGIN.find(" ")
+surname = LOGIN[:spaceIndex + 1]
+first_name = LOGIN[spaceIndex + 1:]
 print(first_name + " " + surname)
 
-""" Напишите программу которая удаляет пробел в начале, в конце строки """
-txt = " Hello World! "
-s = txt.strip()
+# Напишите программу которая удаляет пробел в начале, в конце строки
+HELLO = " Hello World! "
+s = HELLO.strip()
 print(s)
 
-""" Исправьте "pARiS" >> "Paris" """
-txt = "pARiS"
-print(txt.capitalize())
+# Исправьте "pARiS" >> "Paris"
+CITY = "pARiS"
+print(CITY.capitalize())
