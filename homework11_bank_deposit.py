0f1b4e9137576a490e15bd8f6a898b0947d29878
Mariya <by.zorina08@gmail.com>
2024-06-19 13:49:53 +0300

    homework11

--- homework11_bank_deposit.py
+++ homework11_bank_deposit.py
@@ -29,7 +29,7 @@ deposit_2 = Deposit("02.03.2022", "02.03.2025")
 deposit_3 = Deposit("01.01.2025", "31.12.2025")
 
 
-class Bank:  # pylint: disable=too-few-public-methods
+class Bank:
     """Инфорация о договорах пользователя"""
     def __init__(self, deposit_amount, deposit_term_year, percent_deposit):
         self.deposit_amount = deposit_amount
