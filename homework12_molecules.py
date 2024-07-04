16de57c4b6c16f44bf321a27760b9df49a31aad5
Mary-si <by.zorina08@gmail.com>
2024-07-04 12:55:44 +0300

    homework12

--- homework12_molecules.py
+++ homework12_molecules.py
@@ -22,22 +22,24 @@ from collections import Counter
 
 
 def repl(m):
-    """возвращает строко, ктр состоит из символов элемента"""
+    """возвращает строкe, ктр состоит из символов элемента"""
     return m[1] * int(m[2] if m[2] else 1)
 
 
 def molecule(formula: str) -> dict:
     """принимает строку и возвращает словарь с кол-вом элементов"""
+    while "{" in formula:
+        formula = re.sub(r"\{(\w*)}(\d*)", repl, formula)
     while "(" in formula:
-        formula = re.sub(r'\((\w*)\)(\d*)', repl, formula)
+        formula = re.sub(r"\((\w*)\)(\d*)", repl, formula)
     while "[" in formula:
-        formula = re.sub(r'\[(\w*)](\d*)', repl, formula)
-    formula = re.sub(r'([A-Z][a-z]?)(\d*)', repl, formula)
-    formula_dict = Counter(re.findall('[A-Z][a-z]*', formula))
+        formula = re.sub(r"\[(\w*)](\d*)", repl, formula)
+    formula = re.sub(r"([A-Z][a-z]?)(\d*)", repl, formula)
+    formula_dict = Counter(re.findall("[A-Z][a-z]*", formula))
     return formula_dict
 
 
-if __name__ == '__main__':
-    assert molecule("H2O") == {'H': 2, 'O': 1}
-    assert molecule("Mg(OH)2") == {'Mg': 1, 'O': 2, 'H': 2}
-    assert molecule("K4[ON(SO3)2]2") == {'K': 4, 'O': 14, 'N': 2, 'S': 4}
+if __name__ == "__main__":
+    assert molecule("H2O") == {"H": 2, "O": 1}
+    assert molecule("Mg(OH)2") == {"Mg": 1, "O": 2, "H": 2}
+    assert molecule("K4[ON(SO3)2]2") == {"K": 4, "O": 14, "N": 2, "S": 4}
