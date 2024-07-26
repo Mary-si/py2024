c51e6f4aa0ef46880e79d68c62422519db9bf600
Mary-si <by.zorina08@gmail.com>
2024-07-27 01:24:17 +0300

    homework22

--- source/__init__.py
+++ source/__init__.py
@@ -1,5 +1,3 @@
-"""/Users/Mary/Documents/py2024/source/__init__.py"""
-
 """Импортирование необходимых классов и функций из модулей"""
 from .homework11_bank_deposit import Deposit, Bank
 from .homework11_library import Book, User
